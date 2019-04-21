b2a141d7a588c8dc1f690d4ed3651f7b3f81d72b
Kazuki Tsunemi <ksilverwall@gmail.com>
2019-04-21 10:48:52 +0900

    Change client id to staging

--- s3/js/auth.js
+++ s3/js/auth.js
@@ -3,7 +3,7 @@
 class SessionController {
     constructor() {
         this.auth = new AmazonCognitoIdentity.CognitoAuth({
-            ClientId : '1opllluc6ruh50h0qc74395k1i',
+            ClientId : '2kbm6ejg25gk7q3r681nci4b2h',
             AppWebDomain : 'auth.thankshell.com',
             TokenScopesArray : ['profile', 'email', 'openid', 'aws.cognito.signin.user.admin', 'phone'],
             RedirectUriSignIn : 'https://develop.thankshell.com/login/callback',
