7874ca94256cb41cfe80c71233b0a35314907421
Kazuki Tsunemi <ksilverwall@gmail.com>
2019-04-26 09:01:16 +0900

    Add auth-uri and domain switch logic

--- s3/js/auth.js
+++ s3/js/auth.js
@@ -2,13 +2,23 @@
 
 class SessionController {
     constructor() {
-        this.auth = new AmazonCognitoIdentity.CognitoAuth({
-            ClientId : '2kbm6ejg25gk7q3r681nci4b2h',
-            AppWebDomain : 'auth.thankshell.com',
-            TokenScopesArray : ['profile', 'email', 'openid', 'aws.cognito.signin.user.admin', 'phone'],
-            RedirectUriSignIn : 'https://develop.thankshell.com/login/callback',
-            RedirectUriSignOut : 'https://develop.thankshell.com/',
-        });
+        if (document.domain == 'thankshell.com') {
+            this.auth = new AmazonCognitoIdentity.CognitoAuth({
+                ClientId : '1opllluc6ruh50h0qc74395k1i',
+                AppWebDomain : 'auth.thankshell.com',
+                TokenScopesArray : ['profile', 'email', 'openid', 'aws.cognito.signin.user.admin', 'phone'],
+                RedirectUriSignIn : 'https://thankshell.com/login/callback',
+                RedirectUriSignOut : 'https://thankshell.com/',
+            });
+        } else {
+            this.auth = new AmazonCognitoIdentity.CognitoAuth({
+                ClientId : '2kbm6ejg25gk7q3r681nci4b2h',
+                AppWebDomain : 'auth.thankshell.com',
+                TokenScopesArray : ['profile', 'email', 'openid', 'aws.cognito.signin.user.admin', 'phone'],
+                RedirectUriSignIn : 'https://develop.thankshell.com/login/callback',
+                RedirectUriSignOut : 'https://develop.thankshell.com/',
+            });
+        }
     }
 
     close() {
