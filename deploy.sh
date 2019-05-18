eb69f7e2fb2865b3613543712f8975597fb5a593
Kazuki Tsunemi <ksilverwall@gmail.com>
2019-05-18 16:10:21 +0900

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-aws s3 sync --profile thankshell --exclude ".DS_Store" app/ s3://static.thankshell.com/ --delete --acl public-read
+
+set -eu
+
+if [ $1 == "production" ]; then
+   BUCKET_NAME="static.thankshell.com"
+else
+   BUCKET_NAME="develop-static.thankshell.com"
+fi
+
+aws s3 sync --profile thankshell --exclude ".DS_Store" app/ s3://${BUCKET_NAME}/ --delete --acl public-read
